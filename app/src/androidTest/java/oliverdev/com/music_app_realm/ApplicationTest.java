ae19c9a58ebfb74c84fc44da2f2ae7fb20268f42
KanaeToono <lethitrang2910@gmail.com>
2016-08-08 09:59:52 +0700

    source code init project

---
+++ app/src/androidTest/java/oliverdev/com/music_app_realm/ApplicationTest.java
@@ -0,0 +1,13 @@
+package oliverdev.com.music_app_realm;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/oliverdev/com/music_app_realm/MainActivity.java
@@ -0,0 +1,13 @@
+package oliverdev.com.music_app_realm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
